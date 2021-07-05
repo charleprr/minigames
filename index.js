18e8ff039de635e448486b51397539654e63b6d5
charlypoirier <pro.poiriercharly@gmail.com>
2021-07-05 18:45:43 +0200

    Command detection and started leaderboards

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
 const client = new Discord.Client();
+const config = require("./config.json");
 const fs = require("fs");
 
-// Loading games...
+/**
+ * Create and manage leaderboards
+ */
+class Leaderboard {
+
+    constructor (name, highestFirst=true) {
+        this.file = "database/" + name + ".json";
+        this.highestFirst = highestFirst;
+    }
+
+    register (user, score) {
+        const data = JSON.parse(fs.readFileSync(this.file));
+        /* Update data.scores */
+        fs.writeFileSync(this.file, JSON.stringify(data, null, 4));
+    }
+
+    reset () {}
+
+};
+
+/**
+ * Load games
+ */
 const games = new Map();
 fs.readdir("./commands/", (err, files) => {
     if (err) throw err;
@@ -16,13 +38,31 @@ fs.readdir("./commands/", (err, files) => {
     });
 });
 
-// Listen for messages
+
+/**
+ * Listen for messages
+ */
 client.on("message", async m => {
+    if (!m.content.startsWith("!")) return;
     if (m.author.bot) return;
-    if (m.content != "!typerace") return;
-    games.get("typerace").execute(client, m.channel, ()=>{});
+    if (playing) return;
+    
+    let name = m.content.substring(1).split()[0];
+    let game = games.get(name);
+    if (game) {
+        playing = true;
+        game.execute(client, m.channel, (user, score) => {
+            playing = false;
+            /* Register score */
+        });
+    } else {
+        m.channel.send("Unknown game");
+    }
 });
 
-// Startup
+
+/**
+ * Startup
+ */
 client.on("ready", () => client.user.setActivity("around"));
 client.login(config.token);
