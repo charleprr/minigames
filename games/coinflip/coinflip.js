336e45acb12f71654afb4a4f2540f816ccc49cbd
Vistril <38549907+Vistril@users.noreply.github.com>
2021-08-26 13:01:53 -0600

    disgusting

--- games/coinflip/coinflip.js
+++ games/coinflip/coinflip.js
@@ -1,1 +1,10 @@
+export const name = "coinflip"
+export const description = "Flip a coin!"
 
+const coin = () => new Date%2?"Heads!":"Tails!"
+
+export async function execute (interaction) {
+    await interaction.reply("Call it...")
+    await new Promise(r => setTimeout(r, 1600))
+    await interaction.editReply(`**${coin}!**`)
+}
